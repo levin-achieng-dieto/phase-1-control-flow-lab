2c33cd4b17dd5905f675a27984ad1601383a0fcc
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-08-18 00:39:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(distance){
+  let result;
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  if (distance <= 400){
+    result = "This one is on me!"
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  else if(distance > 2000 && distance < 2500){
+    result = "I will gladly take your thirty bucks."
+  }
+  else if (distance > 2500){
+    result = "No can do."
+  }
+
+  return result;
 }
+scuberGreetingForFeet(300)
+
+
+
+function ternaryCheckCity(result){
+  const city = 'NYC';
+  const nice = result===city? "Ok, sounds good." : "No go.";
+  return nice;
+}
+ternaryCheckCity(good)
+
+
+
+function switchOnCharmFromTip(tip){
+  let result;
+switch (tip){
+  case 'generous':
+    result = "Thank you so much.";
+    break;
+    case 'not as generous':
+      result = "Thank you."
+      break;
+      default:
+        result = "Bye."
+}
+return result;
+}
+switchOnCharmFromTip(yess)
